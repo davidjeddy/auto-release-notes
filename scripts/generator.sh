a5d5bf43fd637803574bd72c9431466fecf21148
David J Eddy <me@davidjeddy.com>
2018-01-21 18:52:18 -0500

    REMOVED var_print() from generators.sh

--- scripts/generator.sh
+++ scripts/generator.sh
@@ -2,24 +2,9 @@
 
 # Source: http://brettterpstra.com/2014/08/03/shell-tricks-changelogs-with-git/
 
-# TODO would like to print the var literal AND the value
-function var_print() {
-    varName=$1
-    var_name=(${!varName@})
-    echo "$varName: "+$1
-}
-
-#git rev-list --tags --max-count=1
-#git show -s --format=%ad `git rev-list --tags --max-count=1`
-#lastdate=$(git show -s --format=%ad `git rev-list --tags --max-count=1`)
-#git log --oneline --pretty=format:"%ad: %s" --date=short --since="$lastdate"
-#git log --pretty=format:"- %s%n%b" --since="$lastdate"
-
 latestTagRev=(`git rev-list --tags --max-count=1`)
-var_print $latestTagRev
 
 latestTag=(`git describe --exact-match --tags $latestTagRev`)
-var_print $latestTag
 
 # add the release tag
 echo "#$latestTag#"$'\r' >> ../release-changes.md
