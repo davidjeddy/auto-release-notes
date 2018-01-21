e9770cc9969e88bce48d1aecfe80ade6375d5272
David J Eddy <me@davidjeddy.com>
2018-01-21 18:51:39 -0500

    WORKING ./scripts/generator.sh to output as desired.

--- scripts/generator.sh
+++ scripts/generator.sh
@@ -2,17 +2,34 @@
 
 # Source: http://brettterpstra.com/2014/08/03/shell-tricks-changelogs-with-git/
 
+# TODO would like to print the var literal AND the value
+function var_print() {
+    varName=$1
+    var_name=(${!varName@})
+    echo "$varName: "+$1
+}
+
 #git rev-list --tags --max-count=1
 #git show -s --format=%ad `git rev-list --tags --max-count=1`
 #lastdate=$(git show -s --format=%ad `git rev-list --tags --max-count=1`)
 #git log --oneline --pretty=format:"%ad: %s" --date=short --since="$lastdate"
 #git log --pretty=format:"- %s%n%b" --since="$lastdate"
 
-latestTagRev=$(`git rev-list --tags --max-count=1`)
-echo $latestTagRev
-#git describe --exact-match $latestTagRev >> ../release-changes.md
-#git log --pretty=format:"- %s%n%b" --since="$(git show -s --format=%ad `git rev-list --tags --max-count=1`)" >> ../release-changes.md
+latestTagRev=(`git rev-list --tags --max-count=1`)
+var_print $latestTagRev
 
+latestTag=(`git describe --exact-match --tags $latestTagRev`)
+var_print $latestTag
+
+# add the release tag
+echo "#$latestTag#"$'\r' >> ../release-changes.md
+
+# add the git list of changes
+git log \
+    --date=short \
+    --pretty=format:"- %s%n%b" \
+    --since="$(git show -s --format=%ad $latestTagRev)" \
+    >> ../release-changes.md
 
 # TOOO
 # Toggle to use leader words as an  array of leader words to look for
