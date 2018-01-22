974c2d7c381809622f975996578c4ecc50638e7c
David J Eddy <me@davidjeddy.com>
2018-01-21 20:04:29 -0500

    ADDED .gitignore UPDATED ./scripts/generator.sh, now properly appends ./release-notes.md with new data (instead of prepending.

--- scripts/generator.sh
+++ scripts/generator.sh
@@ -2,19 +2,29 @@
 
 # Source: http://brettterpstra.com/2014/08/03/shell-tricks-changelogs-with-git/
 
+# ensure output dir exists
+if [ ! -f ./release-changes.md ]; then
+    touch ./release-changes.md
+fi
+
 latestTagRev=(`git rev-list --tags --max-count=1`)
 
 latestTag=(`git describe --exact-match --tags $latestTagRev`)
 
 # add the release tag
-echo "#$latestTag#"$'\r' >> ../release-changes.md
+echo $'\r'"#$latestTag#"$'\r' > ./changeTitle
 
 # add the git list of changes
 git log \
     --date=short \
     --pretty=format:"- %s%n%b" \
     --since="$(git show -s --format=%ad $latestTagRev)" \
-    >> ../release-changes.md
+    > ./changeBody
+
+# TODO check if version changes already exists: Remove title and release notes if yes.
+
+cat ./changeTitle ./changeBody ./release-changes.md >out && mv out ./release-changes.md
+rm ./changeTitle && rm ./changeBody
 
 # TOOO
 # Toggle to use leader words as an  array of leader words to look for
