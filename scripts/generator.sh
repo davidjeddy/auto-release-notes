7d1d6f906d81dd5930b636fbe901e3516361305b
David J Eddy <me@davidjeddy.com>
2018-01-21 20:38:07 -0500

    ADDED first argument to specify path to project from script location.

--- scripts/generator.sh
+++ scripts/generator.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+# Source: http:/brettterpstra.com/2014/08/03/shell-tricks-changelogs-with-git/
 
-# Source: http://brettterpstra.com/2014/08/03/shell-tricks-changelogs-with-git/
+# check for script argument 1
+PRJROOT=$PWD
+if [ $# -ne 0 ]
+  then
+    PRJROOT=$1
+fi
+
+# change to specific project root
+cd $PRJROOT
 
 # ensure output dir exists
 if [ ! -f ./release-changes.md ]; then
@@ -11,8 +20,10 @@ latestTagRev=(`git rev-list --tags --max-count=1`)
 
 latestTag=(`git describe --exact-match --tags $latestTagRev`)
 
+echo $"#CHANGELOG"$'\r' > ./doctitle
+
 # add the release tag
-echo $'\r'"#$latestTag#"$'\r' > ./changeTitle
+echo $'\r'"## $latestTag"$'\r' > ./changeTitle
 
 # add the git list of changes
 git log \
@@ -23,8 +34,8 @@ git log \
 
 # TODO check if version changes already exists: Remove title and release notes if yes.
 
-cat ./changeTitle ./changeBody ./release-changes.md >out && mv out ./release-changes.md
-rm ./changeTitle && rm ./changeBody
+cat ./doctitle ./changeTitle ./changeBody ./release-changes.md >out && mv out ./release-changes.md
+rm ./doctitle && rm ./changeTitle && rm ./changeBody
 
 # TOOO
 # Toggle to use leader words as an  array of leader words to look for
